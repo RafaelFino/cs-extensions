393767a72b6934730ff31d1e16f03b6b95cd0202
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-04-05 16:39:34 -0300

    gave up and used tables.

---
+++ Extensions/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Extensions.utils;
+
+using (var ctx = new Context("My function Xpto"))
+{
+    Console.WriteLine("Doing some work...");    
+    Thread.Sleep(1000);
+}
+
+using (var ctx = new Context("My function Ypto"))
+{
+    Console.WriteLine("Doing other some work...");    
+    Thread.Sleep(2000);
+}
